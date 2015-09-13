71bef9591a329ad3b4464196c8920b7afc1a2675
Becky Smith <rebkwok@gmail.com>
2015-09-13 13:08:55 +0100

    webpage refactoring

---
+++ website/context_processors.py
@@ -0,0 +1,28 @@
+from website.models import Page
+
+
+def website_pages(request):
+    pages = Page.objects.all()
+    return {'website_pages': pages}
+
+
+def more_menu_options(request):
+    """
+    :param request:
+    :return: True if there are website pages to be shown in the "More"
+    dropdown menu
+    """
+    pages = Page.objects.all()
+    more_menu_options = [
+        True for page in pages if page.menu_location == 'dropdown'
+        ]
+    return {'more_menu_options': True if more_menu_options else False}
+
+def menu_options(request):
+    """
+    :param request:
+    :return: True if there are any website pages to be shown in the menu bar
+    """
+    pages = Page.objects.all()
+    menu_options = [True for page in pages if page.menu_name]
+    return {'menu_options': True if menu_options else False}
