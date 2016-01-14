215a9d4db14ee912066d6ff0b6a96bbdee31e262
Becky Smith <rebkwok@gmail.com>
2016-01-14 18:25:25 +0000

    Manage command to create default about page

---
+++ website/management/commands/create_about_page.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from website.models import Page
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        page, created = Page.objects.get_or_create(
+            name='about',
+        )
+        if created:
+            page.menu_location = 'main'
+            page.layout = 'no-img'
+            page.content = 'Coming Soon'
+            page.save()
+
+        if created:
+            self.stdout.write('About page created.')
+        else:
+            self.stdout.write('About page already exists.')
